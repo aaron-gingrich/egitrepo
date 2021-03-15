396a5156f35f56045dbbe17c6941b29ff727c0d8
Aaron Gingrich <gingrich.aaron1@gmail.com>
2021-03-15 14:51:23 -0500

    This is adding a second line

--- EgitTesting/src/HellloWorld.java
+++ EgitTesting/src/HellloWorld.java
@@ -4,6 +4,7 @@ public class HellloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("New Line has been added");
 	}
 
 }
